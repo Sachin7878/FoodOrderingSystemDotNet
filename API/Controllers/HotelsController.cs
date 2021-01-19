51874c6162315acc68f4e6e0a0e6bee07e707aac
Sachin Sheelavant <sheelavantsachin@gmail.com>
2021-01-19 13:12:45 +0530

    added menuitems crud operations

--- API/Controllers/HotelsController.cs
+++ API/Controllers/HotelsController.cs
@@ -99,12 +99,92 @@ namespace API.Controllers
             _context.Hotels.Remove(hotel);
             await _context.SaveChangesAsync();
 
-            return hotel;
+            return Ok();
         }
+
+        //[HttpPost("{id}/image")]
+        //public async Task<ActionResult<Hotel>> PostImage(long id, FormFile imageFile)
+        //{
+        //    var hotel = await _context.Hotels.FindAsync(id);
+        //    if (hotel == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    imageFile.
+
+        //    return null;
+        //}
+
+        //Menu Items CRUD
+
+        [HttpGet("{id}/menu/")]
+        public OkObjectResult GetMenuItems(long id)
+        {
+            var menuItems = _context.MenuItems.Where(i => i.Hotel.Id == id);
+            return Ok(new { Content = menuItems });
+        }
+
+        [HttpPost("{id}/menu/")]
+        public async Task<ActionResult<Hotel>> PostMenuItem(long id, MenuItem item)
+        {
+            await _context.MenuItems.AddAsync(item);
+            await _context.SaveChangesAsync();
+
+            return Ok(item);
+        }
+
+        // DELETE: api/Hotels/5/menu/1
+        [HttpDelete("{hotelId}/menu/{menuId}")]
+        public async Task<ActionResult<Hotel>> DeleteHotel(long hotelId, long menuId)
+        {
+            var menuItem = _context.MenuItems.Where(i => i.Hotel.Id == hotelId).First(m => m.Id == menuId);
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+            _context.MenuItems.Remove(menuItem);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{hotelId}/menu/{menuId}")]
+        public async Task<IActionResult> PutMenuItem(long hotelId, long menuId, MenuItem item)
+        {
+            if (menuId != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelExists(hotelId) && !MenuItemExists(menuId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        //helper methods
 
         private bool HotelExists(long id)
         {
             return _context.Hotels.Any(e => e.Id == id);
+        }
+        private bool MenuItemExists(long id)
+        {
+            return _context.MenuItems.Any(e => e.Id == id);
         }
     }
 }
